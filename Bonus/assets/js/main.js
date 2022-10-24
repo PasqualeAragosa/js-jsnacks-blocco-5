406371880a10cc09ddccc7779446cdd41cc0a1d7
Pasquale Aragosa <aragosap@gmail.com>
2022-10-24 17:50:37 +0200

    add bonus

---
+++ Bonus/assets/js/main.js
@@ -0,0 +1,46 @@
+/*
+Bonus:
+Crea un array di oggetti che rappresentano delle persone.
+Ogni persona ha un nome, un cognome e un’età.
+Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l’indicazione se può guidare, in base all’età.
+*/
+
+const users = [
+    {
+        name: 'Pasquale',
+        surname: 'Aragosa',
+        age: 24
+    },
+    {
+        name: 'Matteo',
+        surname: 'Aragosa',
+        age: 17
+    },
+    {
+        name: 'Angelo',
+        surname: 'Aragosa',
+        age: 26
+    },
+    {
+        name: 'Biagio',
+        surname: 'Aragosa',
+        age: 14
+    }
+];
+
+let newUsers = [];
+
+
+users.forEach((element, index) => {
+
+    if (users[index].age >= 18) {
+        newMarkup = `${users[index].name} ${users[index].surname} può guidare`;
+        newUsers.push(newMarkup);
+    } else {
+        newMarkup = `${users[index].name} ${users[index].surname} non può ancora guidare`;
+        newUsers.push(newMarkup);
+    }
+}
+);
+
+console.log(newUsers);
